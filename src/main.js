8daa53c213c2797d93e9959b2d4f85516fe56d08
rinnirt@hotmail.com <rin@Rins-MacBook-Pro.local>
2017-06-17 23:24:50 +0700

    add code firebase cloud message api

--- src/main.js
+++ src/main.js
@@ -4,6 +4,98 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import * as VueGoogleMaps from 'vue2-google-maps'
+import * as firebase from 'firebase'
+
+var firebaseConfig = {
+  apiKey: 'AIzaSyBwdEQvn2bT5jlZ4xG8tdg4r97GHdxutJY',
+  projectId: 'lab2-4d0bb',
+  authDomain: 'lab2-4d0bb.firebaseapp.com',
+  databaseURL: 'https://lab2-4d0bb.firebaseio.com',
+  storageBucket: 'lab2-4d0bb.appspot.com',
+  messagingSenderId: '623837882946'
+}
+
+firebase.initializeApp(firebaseConfig)
+
+const messaging = firebase.messaging()
+
+messaging.requestPermission()
+.then(function () {
+  console.log('Notification permission granted.')
+  // TODO(developer): Retrieve an Instance ID token for use with FCM.
+  // ...
+  getToken()
+})
+.catch(function (err) {
+  console.log('Unable to get permission to notify.', err)
+})
+
+function getToken () {
+  messaging.getToken()
+  .then(function (currentToken) {
+    if (currentToken) {
+      console.log(currentToken)
+      sendTokenToServer(currentToken)
+      // updateUIForPushEnabled(currentToken)
+    } else {
+      // Show permission request.
+      console.log('No Instance ID token available. Request permission to generate one.')
+      // Show permission UI.
+      // updateUIForPushPermissionRequired()
+      setTokenSentToServer(false)
+    }
+  })
+  .catch(function (err) {
+    console.log('An error occurred while retrieving token. ', err)
+    // showToken('Error retrieving Instance ID token. ', err)
+    setTokenSentToServer(false)
+  })
+}
+
+  // Callback fired if Instance ID token is updated.
+messaging.onTokenRefresh(function () {
+  messaging.getToken()
+  .then(function (refreshedToken) {
+    console.log('Token refreshed.')
+    // Indicate that the new Instance ID token has not yet been sent to the
+    // app server.
+    // setTokenSentToServer(false);
+    // Send Instance ID token to app server.
+    // sendTokenToServer(refreshedToken);
+    // ...
+  })
+  .catch(function (err) {
+    console.log('Unable to retrieve refreshed token ', err)
+    // showToken('Unable to retrieve refreshed token ', err)
+  })
+})
+
+messaging.onMessage(function (payload) {
+  console.log('Message received. ', payload)
+  // ...
+})
+
+// Send the Instance ID token your application server, so that it can:
+// - send messages back to this app
+// - subscribe/unsubscribe the token from topics
+function sendTokenToServer (currentToken) {
+  if (!isTokenSentToServer()) {
+    console.log('Sending token to server...')
+    // TODO(developer): Send the current token to your server.
+    setTokenSentToServer(true)
+  } else {
+    console.log('Token already sent to server so won\'t send it again ' +
+        'unless it changes')
+  }
+}
+
+function isTokenSentToServer () {
+  return window.localStorage.getItem('sentToServer') === 1
+}
+
+function setTokenSentToServer (sent) {
+  window.localStorage.setItem('sentToServer', sent ? 1 : 0)
+}
 
 Vue.config.productionTip = false
 
@@ -35,3 +127,4 @@ new Vue({
   template: '<App/>',
   components: { App }
 })
+
